a0dee34367285c2958131a38a589ff847d2f1b1d
Chanuka Gunadasa <chanukag@zone24x7.com>
2020-08-24 11:14:47 +0530

    rollup version:1.29.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,29 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
+import path from 'path'
+import postcss from 'rollup-plugin-postcss'
 
 export default [
-	// browser-friendly UMD build
 	{
 		input: 'src/main.js',
 		output: {
-			name: 'howLongUntilLunch',
+			name: 'my-faceapi-js-lib',
 			file: pkg.browser,
 			format: 'umd'
 		},
 		plugins: [
+			postcss({
+				minimize: true,
+				extensions: ['.css'],
+				extract: path.resolve('dist/my-faceapi-js-lib.css'),
+			}),
 			resolve(), // so Rollup can find `ms`
 			commonjs() // so Rollup can convert `ms` to an ES module
 		]
 	},
-
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
-	// `file` and `format` for each target)
 	{
 		input: 'src/main.js',
-		external: ['ms'],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
